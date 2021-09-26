875300b9de71f15055317405e4e66e65588d13e6
aldiandya <82937781+aldiandya@users.noreply.github.com>
2021-09-26 12:59:17 +0700

    update allowing env

--- models/db_connect.go
+++ models/db_connect.go
@@ -4,6 +4,7 @@ package models
 import (
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
@@ -13,11 +14,20 @@ var DB *gorm.DB
 
 // SetupDB : initializing mysql database
 func ConnectDB() {
-	USER := "jcc_blogs"
-	PASS := "ZAqJ_aFa6EZi26-r"
-	HOST := "localhost"
-	PORT := "3306"
-	DBNAME := "jcc_blogs"
+	USER := os.Getenv("DB_USER")
+	PASS := os.Getenv("DB_PASS")
+	HOST := os.Getenv("DB_HOST")
+	PORT := os.Getenv("DB_PORT")
+	DBNAME := os.Getenv("DB_NAME")
+
+	if USER == "" {
+		// use default
+		USER = "jcc_blogs"
+		PASS = "ZAqJ_aFa6EZi26-r"
+		HOST = "localhost"
+		PORT = "3306"
+		DBNAME = "jcc_blogs"
+	}
 
 	// GORM using DSN Format
 	DSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
